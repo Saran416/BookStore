d4b5c8c6799fc0df0bd071c6818d8e25555e4875
Saran416 <saisriramasarankonala@gmail.com>
2024-06-16 21:33:40 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBook from './pages/ShowBook'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home></Home>}>
+        </Route>
+        <Route path='/books/create' element={<CreateBook></CreateBook>}>
+        </Route>
+        <Route path='/books/details/:id' element={<ShowBook></ShowBook>}>
+        </Route>
+        <Route path='/books/edit/:id' element={<EditBook></EditBook>}>
+        </Route>
+        <Route path='/books/delete/:id' element={<DeleteBook></DeleteBook>}>
         </Route>
       </Routes>
     </BrowserRouter>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import './Home.css'
 
 const Home = () => {
   const [books,setBooks] = useState([])
@@ -8,21 +10,33 @@ const Home = () => {
     axios.get('http://localhost:5000/books')
     .then((res)=>{
       setBooks(res.data.data)
+      console.log(res.data.data)
     })
     .catch((err)=>{
       console.log(err)
     })
   },[])
+
   return (
     <div className="box">
-      <h1>Books List</h1>
-      <ol>
-        {books.map((book,index)=>{<ul key={index}>
-          <li> Name: {book.name} </li>
-          <li> Author: {book.author}</li>
-          <li> Published in the year {book.publishYear}</li>
-        </ul>})}
-      </ol>
+      <div className='nav'>
+        <h1>Books List</h1>
+        <Link to='/books/create'><button>+</button></Link>
+      </div>
+        <ol>
+          <li>
+          {books.map((book)=> 
+            <ul key={book._id} className='item'>
+              <li> Title: {book.title} </li>
+              <li> Author: {book.author}</li>
+              <li> Published in the year {book.publishYear}</li>
+              <li><Link to={`books/details/${book._id}`}><button>Book Details</button></Link></li>
+              <li><Link to={`books/edit/${book._id}`}><button>Edit Book</button></Link></li>
+              <li><Link to={`books/delete/${book._id}`}><button>Delete Book</button></Link></li>     
+            </ul>
+      )}
+          </li>
+        </ol>
     </div>
   )
 }
